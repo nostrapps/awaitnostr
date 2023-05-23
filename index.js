929aaaf5cabfbff84d33c725cf475227b5e07452
Melvin Carvalho <melvincarvalho@gmail.com>
2023-05-23 15:01:18 +0200

    add jsdoc

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+/**
+ * Waits for the `nostr` object to be defined on the `window` object.
+ * @returns {Promise<void>} A promise that resolves when `nostr` is defined or when the maximum elapsed time is reached.
+ */
 export default function awaitNostr () {
   return new Promise(resolve => {
     let intervalTime = 2
